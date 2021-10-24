4206d907c72873e86af7f2a333a6471cd995f171
Matas <luckyrads@gmail.com>
2021-10-24 23:10:40 +0300

    REM: Project.

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
--- app/types/response_types.py
+++
@@ -1,8 +0,0 @@
-class BasicResponse:
-    status: str = ''
-    message: str = ''
-
-    def __init__(self, status: str, message: str) -> None:
-        self.status = status
-        self.message = message
-        return {'status': status, 'message': message}
